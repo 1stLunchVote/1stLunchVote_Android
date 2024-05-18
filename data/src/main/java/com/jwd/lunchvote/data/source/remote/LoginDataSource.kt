96fd2e0d937c330fcc57b3d883acd97f739a30f3
김태우 <t.wooo@kakao.com>
2024-05-18 17:25:59 +0900

    [#57] - refactor: LoginRepository, LoginDataSource 수정

--- data/src/main/java/com/jwd/lunchvote/data/source/remote/LoginDataSource.kt
+++ data/src/main/java/com/jwd/lunchvote/data/source/remote/LoginDataSource.kt
@@ -1,6 +1,7 @@
 package com.jwd.lunchvote.data.source.remote
 
 interface LoginDataSource {
+
   suspend fun createUserWithEmailAndPassword(email: String, password: String): String
   suspend fun signInWithEmailAndPassword(email: String, password: String): String
   suspend fun signInWithKakaoIdToken(idToken: String): String
