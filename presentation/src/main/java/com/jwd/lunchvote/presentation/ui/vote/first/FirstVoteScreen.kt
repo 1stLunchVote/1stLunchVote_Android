a0ce4c6c8ad1cfccfdbe6ac88bd726be07c2dcd8
김태우 <t.wooo@kakao.com>
2024-05-29 22:47:42 +0900

    [#60] - feat: HorizontalProgressBar 구현

--- presentation/src/main/java/com/jwd/lunchvote/presentation/ui/vote/first/FirstVoteScreen.kt
+++ presentation/src/main/java/com/jwd/lunchvote/presentation/ui/vote/first/FirstVoteScreen.kt
@@ -2,21 +2,13 @@ package com.jwd.lunchvote.presentation.ui.vote.first
 
 import android.content.Context
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -24,22 +16,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.jwd.lunchvote.core.ui.theme.LunchVoteTheme
 import com.jwd.lunchvote.presentation.R
-import com.jwd.lunchvote.presentation.model.FoodStatus
-import com.jwd.lunchvote.presentation.model.FoodUIModel
 import com.jwd.lunchvote.presentation.ui.vote.first.FirstVoteContract.FirstVoteEvent
 import com.jwd.lunchvote.presentation.ui.vote.first.FirstVoteContract.FirstVoteSideEffect
 import com.jwd.lunchvote.presentation.ui.vote.first.FirstVoteContract.FirstVoteState
-import com.jwd.lunchvote.presentation.widget.FoodItem
-import com.jwd.lunchvote.presentation.widget.LikeDislike
+import com.jwd.lunchvote.presentation.widget.HorizontalProgressBar
 import com.jwd.lunchvote.presentation.widget.LoadingScreen
-import com.jwd.lunchvote.presentation.widget.LunchVoteTextField
-import com.jwd.lunchvote.presentation.widget.ProgressTopBar
+import com.jwd.lunchvote.presentation.widget.LunchVoteTopBar
 import com.jwd.lunchvote.presentation.widget.Screen
 import com.jwd.lunchvote.presentation.widget.ScreenPreview
-import com.jwd.lunchvote.presentation.widget.StepProgress
-import com.jwd.lunchvote.presentation.widget.TextFieldType
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
@@ -59,10 +44,10 @@ fun FirstVoteRoute(
   LaunchedEffect(viewModel.sideEffect) {
     viewModel.sideEffect.collectLatest {
       when(it) {
+        is FirstVoteSideEffect.PopBackStack -> popBackStack()
         is FirstVoteSideEffect.NavigateToSecondVote -> navigateToSecondVote()
         is FirstVoteSideEffect.OpenTemplateDialog -> openTemplateDialog()
         is FirstVoteSideEffect.OpenVoteExitDialog -> openVoteExitDialog()
-        is FirstVoteSideEffect.PopBackStack -> popBackStack()
         is FirstVoteSideEffect.ShowSnackBar -> showSnackBar(it.message.asString(context))
       }
     }
@@ -85,9 +70,26 @@ private fun FirstVoteScreen(
   onEvent: (FirstVoteEvent) -> Unit = {}
 ) {
   Screen(
-    modifier = modifier
+    modifier = modifier,
+    topAppBar = {
+      LunchVoteTopBar(
+        title = stringResource(R.string.first_vote_title),
+        navIconVisible = false
+      )
+      HorizontalProgressBar(
+        timeLimitSecond = 60,
+        modifier = Modifier.fillMaxWidth()
+      )
+    }
   ) {
+    Column(
+      modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = 32.dp)
+        .padding(top = 16.dp)
+    ) {
 
+    }
   }
 }
 
