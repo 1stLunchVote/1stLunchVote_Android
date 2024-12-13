34dc47ce8ded1df778c4dadd7b46f648ebb62475
김태우 <t.wooo@kakao.com>
2024-12-13 14:53:46 +0900

    [241125] - color: reindent

--- presentation/src/main/java/com/jwd/lunchvote/presentation/theme/Shape.kt
+++ presentation/src/main/java/com/jwd/lunchvote/presentation/theme/Shape.kt
@@ -7,9 +7,9 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun lunchVoteShapes() = MaterialTheme.shapes.copy(
-    extraSmall = RoundedCornerShape(4.dp),
-    small = RoundedCornerShape(8.dp),
-    medium = RoundedCornerShape(16.dp),
-    large = RoundedCornerShape(24.dp),
-    extraLarge = RoundedCornerShape(100)
+  extraSmall = RoundedCornerShape(4.dp),
+  small = RoundedCornerShape(8.dp),
+  medium = RoundedCornerShape(16.dp),
+  large = RoundedCornerShape(24.dp),
+  extraLarge = RoundedCornerShape(100)
 )
